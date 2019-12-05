650ac10db5af7ca7ba3924cb5d44a1c98dc5bcf1
FrozenFish24 <FrozenFish24@users.noreply.github.com>
2019-12-05 21:17:33 +0800

    legacy/map_helper.py: use more idiomatic python function and variable names

--- legacy/map_helper.py
+++ legacy/map_helper.py
@@ -8,218 +8,219 @@ PRIM_IND = 1
 VB_INDEX = 0
 
 VERTS_ADDED = 6
-VERT_SIZE = 0xC0
+VERT_LEN = 0xC0
 
-ORIG_IB_SIZE = 0x159C8
-NEW_IB_SIZE = 0x159DA
-IB_DIF = NEW_IB_SIZE - ORIG_IB_SIZE
+ORIG_IB_LEN = 0x159C8
+NEW_IB_LEN = 0x159DA
+IB_DIF = NEW_IB_LEN - ORIG_IB_LEN
 
 INDEXES_ADDED = IB_DIF // 2
 
 def main():
     with open('%s' % map_name, 'rb+') as f:
-        objOffsetList = getObjectOffsets(f)
-        UpdateObject(f, objOffsetList[OBJECT_IND])
+        object_offsets = get_object_offsets(f)
+        update_object(f, object_offsets[OBJECT_IND])
 
-def getObjectOffsets(f):
-        objOffsetList = []
+def get_object_offsets(f):
+        object_offsets = []
 
         f.read(4)
-        mapSize = f.read(0x4)
-        mapSize = struct.unpack('<I', mapSize)[0]
-        print('MAP SIZE:\t\t0x%X' % mapSize)
-        mapSize += VERT_SIZE + IB_DIF
-        f.seek(-4,1)
-        f.write(struct.pack('<I', mapSize))
+        map_len = f.read(0x4)
+        map_len = struct.unpack('<I', map_len)[0]
+        print(f'MAP LEN:\t\t0x{map_len:X}')
+        map_len += VERT_LEN + IB_DIF
+        f.seek(-4, 1)
+        f.write(struct.pack('<I', map_len))
         #UPDATE VALUE HERE
         
         f.read(0xC) #ignore irrelevant data for now
-        texSectionLen = f.read(4)
-        texSectionLen = struct.unpack('<I', texSectionLen)[0]
+        tex_section_len = f.read(4)
+        tex_section_len = struct.unpack('<I', tex_section_len)[0]
         f.read(8)
-        f.seek(texSectionLen, 1)
+        f.seek(tex_section_len, 1)
 
         f.read(4)
-        sectionSize = f.read(4)
-        sectionSize = struct.unpack('<I', sectionSize)[0]
-        print('SECTION SIZE:\t\t0x%X' % sectionSize)
-        sectionSize += VERT_SIZE + IB_DIF
-        f.seek(-4,1)
-        f.write(struct.pack('<I', sectionSize))
+        section_len = f.read(4)
+        section_len = struct.unpack('<I', section_len)[0]
+        print(f'SECTION LEN:\t\t0x{section_len:X}')
+        section_len += VERT_LEN + IB_DIF
+        f.seek(-4, 1)
+        f.write(struct.pack('<I', section_len))
         #UPDATE VALUE HERE
         
         f.read(0xC) # discad geom section header and creation date
-        objCount = f.read(4)
-        objCount = struct.unpack('<I', objCount)[0]
+        object_count = f.read(4)
+        object_count = struct.unpack('<I', object_count)[0]
 
-        subSectionSize = f.read(4)
-        subSectionSize = struct.unpack('<I', subSectionSize)[0]
-        print('SUBSECTION SIZE:\t0x%X' % subSectionSize)
-        subSectionSize += VERT_SIZE + IB_DIF
-        f.seek(-4,1)
-        f.write(struct.pack('<I', subSectionSize))
+        sub_section_len = f.read(4)
+        sub_section_len = struct.unpack('<I', sub_section_len)[0]
+        print(f'SUBSECTION LEN:\t0x{sub_section_len:X}')
+        sub_section_len += VERT_LEN + IB_DIF
+        f.seek(-4, 1)
+        f.write(struct.pack('<I', sub_section_len))
         #UPDATE VALUE HERE
         
         f.read(4)
 
-        while(objCount > 0):
+        while(object_count > 0):
 
-            objStart = f.tell()
+            object_start = f.tell()
             f.read(4)
-            objSize = f.read(4)
-            objSize = struct.unpack('<I', objSize)[0]
+            object_len = f.read(4)
+            object_len = struct.unpack('<I', object_len)[0]
 
-            objOffsetList.append(objStart)
+            object_offsets.append(object_start)
             
-            f.seek(objStart + objSize)
-            objCount -= 1
+            f.seek(object_start + object_len)
+            object_count -= 1
 
-        return objOffsetList
+        #return objOffsetList
+        return [object_offsets[0], object_offsets[1], object_offsets[3]]
 
-def UpdateObject(f, off):
-    vertBufOffsets = []
+def update_object(f, off):
+    vb_offsets = []
 
     f.seek(off)
 
     f.read(4)
-    objectSize = f.read(4)
-    objectSize = struct.unpack('<I', objectSize)[0]
-    print('OBJECT SIZE:\t\t0x%X' % objectSize)
-    objectSize += VERT_SIZE + IB_DIF
-    f.seek(-4,1)
-    f.write(struct.pack('<I', objectSize))
+    object_len = f.read(4)
+    object_len = struct.unpack('<I', object_len)[0]
+    print(f'OBJECT LEN:\t\t0x{object_len:X}')
+    object_len += VERT_LEN + IB_DIF
+    f.seek(-4, 1)
+    f.write(struct.pack('<I', object_len))
     # UPDATE VALUE HERE
     
     f.read(4)
 
-    unkSizeNEG1 = f.read(4)
-    unkSizeNEG1 = struct.unpack('<I', unkSizeNEG1)[0]
-    print('UNK-1 SIZE:\t\t0x%X' % unkSizeNEG1)
-    if(unkSizeNEG1 != 0): # Only non-zero in streamed maps
-        unkSizeNEG1 += VERT_SIZE + IB_DIF
-    f.seek(-4,1)
-    f.write(struct.pack('<I', unkSizeNEG1))
-    # UPDATE VALUE HERE (ALSO NEEDS PROPER VAR NAME)
+    unk_len_0 = f.read(4)
+    unk_len_0 = struct.unpack('<I', unk_len_0)[0]
+    print(f'UNK0 LEN:\t\t0x{unk_len_0:X}')
+    if(unk_len_0 != 0): # Only non-zero in streamed maps
+        unk_len_0 += VERT_LEN + IB_DIF
+    f.seek(-4, 1)
+    f.write(struct.pack('<I', unk_len_0))
+    # UPDATE VALUE HERE
 
-    unkSize0 = f.read(4)
-    unkSize0 = struct.unpack('<I', unkSize0)[0]
-    print('UNK0 SIZE:\t\t0x%X' % unkSize0)
-    unkSize0 += VERT_SIZE + IB_DIF
-    f.seek(-4,1)
-    f.write(struct.pack('<I', unkSize0))
+    unk_len_1 = f.read(4)
+    unk_len_1 = struct.unpack('<I', unk_len_1)[0]
+    print(f'UNK1 len:\t\t0x{unk_len_1:X}')
+    unk_len_1 += VERT_LEN + IB_DIF
+    f.seek(-4, 1)
+    f.write(struct.pack('<I', unk_len_1))
     # UPDATE VALUE HERE
 
     f.read(4)
     
-    cullVolumeSz = f.read(4)
-    cullVolumeSz = struct.unpack('<I', cullVolumeSz)[0]
+    bounding_volume_len = f.read(4)
+    bounding_volume_len = struct.unpack('<I', bounding_volume_len)[0]
 
-    f.read(cullVolumeSz * 4) # ignore occlusion culling volume
+    f.read(bounding_volume_len * 4) # ignore occlusion culling volume
 
     f.read(4) # ignore whatever this is
 
     index_buf_ptr = f.read(4)
     index_buf_ptr = struct.unpack('<I', index_buf_ptr)[0]
-    print('INDEX BUF POINTER:\t0x%X' % index_buf_ptr)
-    index_buf_ptr += VERT_SIZE
-    f.seek(-4,1)
+    print(f'INDEX BUF POINTER:\t0x{index_buf_ptr:X}')
+    index_buf_ptr += VERT_LEN
+    f.seek(-4, 1)
     f.write(struct.pack('<I', index_buf_ptr))
     # UPDATE VALUE HERE
     
-    index_buf_sz = f.read(4)
-    index_buf_sz = struct.unpack('<I', index_buf_sz)[0]
-    print('INDEX BUF SIZE:\t\t0x%X' % index_buf_sz)
-    index_buf_sz += IB_DIF
-    f.seek(-4,1)
-    f.write(struct.pack('<I', index_buf_sz))
+    index_buf_len = f.read(4)
+    index_buf_len = struct.unpack('<I', index_buf_len)[0]
+    print(f'INDEX BUF LEN:\t\t0x{index_buf_len:X}')
+    index_buf_len += IB_DIF
+    f.seek(-4, 1)
+    f.write(struct.pack('<I', index_buf_len))
     # UPDATE VALUE HERE
 
-    unkSize1 = f.read(4)
-    unkSize1 = struct.unpack('<I', unkSize1)[0]
-    print('UNK1 SIZE:\t\t0x%X' % unkSize1)
-    unkSize1 += VERT_SIZE + IB_DIF
-    f.seek(-4,1)
-    f.write(struct.pack('<I', unkSize1))
+    unk_len_2 = f.read(4)
+    unk_len_2 = struct.unpack('<I', unk_len_2)[0]
+    print(f'UNK2 LEN:\t\t0x{unk_len_2:X}')
+    unk_len_2 += VERT_LEN + IB_DIF
+    f.seek(-4, 1)
+    f.write(struct.pack('<I', unk_len_2))
     #UPDATE VALUE HERE
 
-    primList = getPrimitives(f)
+    primitive_list = get_primitives(f)
 
-    unkSize2 = f.read(4)
-    unkSize2 = struct.unpack('<I', unkSize2)[0]
-    print('UNK2 SIZE:\t\t0x%X' % unkSize2)
-    unkSize2 += VERT_SIZE
-    f.seek(-4,1)
-    f.write(struct.pack('<I', unkSize2))
+    unk_len_3 = f.read(4)
+    unk_len_3 = struct.unpack('<I', unk_len_3)[0]
+    print(f'UNK3 LEN:\t\t0x{unk_len_3:X}')
+    unk_len_3 += VERT_LEN
+    f.seek(-4, 1)
+    f.write(struct.pack('<I', unk_len_3))
     #UPDATE VALUE HERE
 
     vb_count = f.read(4)
     vb_count = struct.unpack('<I', vb_count)[0]
 
-    vbInfoList = []
+    vb_info_list = []
     for vb in range(0, vb_count):
         if(vb >= VB_INDEX): # SHOULD REALLY GET THE VB INDEX PROGRAMATICALLY
-            vbStart = f.read(4)
-            vbStart = struct.unpack('<I', vbStart)[0]
-            print('VERTBUF START:\t\t0x%X' % vbStart)
+            vb_start = f.read(4)
+            vb_start = struct.unpack('<I', vb_start)[0]
+            print(f'VERTBUF START:\t\t0x{vb_start:X}')
             if(vb > VB_INDEX):
-                vbStart += VERT_SIZE
+                vb_start += VERT_LEN
             f.seek(-4,1)
-            f.write(struct.pack('<I', vbStart))
+            f.write(struct.pack('<I', vb_start))
             #UPDATE VALUE HERE (NOT NECESSARY FOR HP190)
 
             f.read(4) #discard stride
 
-            vbSize = f.read(4)
-            vbSize = struct.unpack('<I', vbSize)[0]
-            print('VERTBUF SIZE:\t\t0x%X' % vbSize)
-            vbSize += VERT_SIZE
+            vb_len = f.read(4)
+            vb_len = struct.unpack('<I', vb_len)[0]
+            print(f'VERTBUF LEN:\t\t0x{vb_len:X}')
+            vb_len += VERT_LEN
             f.seek(-4,1)
-            f.write(struct.pack('<I', vbSize))
+            f.write(struct.pack('<I', vb_len))
             #UPDATE VALUE HERE
         else:
             f.read(12) # skip vertex buffer
 
-def getPrimitives(f):
+def get_primitives(f):
     print()
-    obj_count = f.read(4)
-    obj_count = struct.unpack('<I', obj_count)[0]
+    object_count = f.read(4)
+    object_count = struct.unpack('<I', object_count)[0]
     
-    primList = []
-    for o in range(0, obj_count):
+    primitives = []
+    for o in range(0, object_count):
         if(o >= PRIM_IND):
             f.read(0xC)
 
-            primCount = f.read(2)
-            primCount = struct.unpack('<H', primCount)[0]
-            print('P%d PRIM COUNT:\t\t0x%X' % (o, primCount))
+            primitive_count = f.read(2)
+            primitive_count = struct.unpack('<H', primitive_count)[0]
+            print(f'P{o} PRIM COUNT:\t\t0x{primitive_count:X}')
             if(o == PRIM_IND):
-                primCount += INDEXES_ADDED
+                primitive_count += INDEXES_ADDED
                 f.seek(-2,1)
-                f.write(struct.pack('<H', primCount))
+                f.write(struct.pack('<H', primitive_count))
                 #UPDATE VALUE HERE
 
             f.read(2)
 
-            vertStart = f.read(2)
-            vertStart = struct.unpack('<H', vertStart)[0]
-            print('P%d VERT START:\t\t0x%X' % (o, vertStart))
+            vert_start = f.read(2)
+            vert_start = struct.unpack('<H', vert_start)[0]
+            print(f'P{o} VERT START:\t\t0x{vert_start:X}')
             if(o > PRIM_IND):
-                vertStart += VERTS_ADDED
+                vert_start += VERTS_ADDED
                 f.seek(-2,1)
-                f.write(struct.pack('<H', vertStart))
+                f.write(struct.pack('<H', vert_start))
                 #UPDATE VALUE HERE
 
-            vertEnd = f.read(2)
-            vertEnd = struct.unpack('<H', vertEnd)[0]
-            print('P%d VERT END:\t\t0x%X' % (o, vertEnd))
-            vertEnd += VERTS_ADDED
+            vert_end = f.read(2)
+            vert_end = struct.unpack('<H', vert_end)[0]
+            print(f'P{o} VERT END:\t\t0x{vert_end:X}')
+            vert_end += VERTS_ADDED
             f.seek(-2,1)
-            f.write(struct.pack('<H', vertEnd))
+            f.write(struct.pack('<H', vert_end))
             #UPDATE VALUE HERE
         else:
             f.read(0x14) # Not interested, skip
         
-    return primList
+    return primitives
 
 if __name__ == '__main__':
     main()
